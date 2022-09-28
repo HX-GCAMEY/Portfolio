2fbd41f57495a170dc402c5dd48f1548de43c0ac
Gama <lic.camey@gmail.com>
2022-09-28 16:47:43 -0300

    PI anadido

--- src/components/MyWork/index.js
+++ src/components/MyWork/index.js
@@ -2,9 +2,11 @@ import './index.scss';
 import { useState, useEffect } from 'react';
 import AnimatedLetter from '../AnimatedLetters';
 import { Loader } from 'react-loaders';
+import pokemonpi from '../../assets/videos/pokemon-pi.mp4';
 
 const MyWork = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
+  let [toggleSize, setToggleSize] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -13,9 +15,21 @@ const MyWork = () => {
     return () => clearTimeout(timer);
   }, []);
 
+  const goToPage = () => {
+    window.open('https://pipoke.vercel.app/');
+  };
+
+  const resizeVideo = () => {
+    if (toggleSize) {
+      setToggleSize(false);
+    } else {
+      setToggleSize(true);
+    }
+  };
+
   return (
     <>
-      <div className="container about-page">
+      <div className="container work-page">
         <div className="text-zone">
           <h1>
             <AnimatedLetter
@@ -29,6 +43,29 @@ const MyWork = () => {
             a part of a team, this projects where made during my learning path
             at Henry!
           </p>
+        </div>
+        <div className="video-zone">
+          <div className={toggleSize ? 'info-video' : 'info-hidden'}>
+            <span>Pokemon PI</span>
+            <br />
+            A fun pokemon website that
+            <br />
+            was a part of a proyect on Henry
+            <br />
+            bootcamp
+            <br />
+            <p onClick={goToPage}>
+              Go to the page
+              <br /> or click the video to enlarge 😼
+            </p>
+          </div>
+          <video
+            src={pokemonpi}
+            autoPlay={true}
+            muted={true}
+            width={toggleSize ? '70%' : '180%'}
+            onClick={resizeVideo}
+          />
         </div>
       </div>
       <Loader type="cube-transition" />
